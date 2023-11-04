206501058ae5496d75fede4b376f02125db00780
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-11-04 21:27:06 +0530

    sweet msg on add items

--- src/Components/Items.js
+++ src/Components/Items.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useSelector,useDispatch } from 'react-redux';
 import { addItem } from '../Redux/Actions/cartAction';
 
+import Swal from 'sweetalert2' // for sweet msg alerts
+
 const Items = ({product}) => {
     const dispatch = useDispatch();
     const data = useSelector(state=>state.cart);
@@ -10,6 +12,13 @@ const Items = ({product}) => {
         data.forEach(element => {
           if(element.id === product.id){
             add = false;
+            Swal.fire({
+              position: 'top-end',
+              icon: 'error',
+              title: 'Item already added',
+              showConfirmButton: false,
+              timer: 1200
+            });
           } 
         });
         if(add){
@@ -17,6 +26,14 @@ const Items = ({product}) => {
           if(localStorage.getItem('cart-items')){
             const tempArr = JSON.parse(localStorage.getItem('cart-items'));
             localStorage.setItem('cart-items',JSON.stringify([...tempArr,product]));
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: 'Your item is added to the cart',
+              showConfirmButton: false,
+              timer: 1200
+            });
+
           }else{
             localStorage.setItem('cart-items',JSON.stringify([product]));
           }
